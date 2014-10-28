83e72f4d9574fc577dae12825a5c58fa3c691a51
Clinton Best <clintguy45@gmail.com>
2014-10-28 20:52:44 +0000

    trying to fix hyperlink

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ActionMailer::Base
+  #encoding: utf-8
   default from: "noreply@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -8,7 +9,11 @@ class UserMailer < ActionMailer::Base
   #
   def account_activation(user)
     @user = user
+    from    "C.Best Ideas<notifications@example.com>"
     mail to: user.email, subject: "Account Activation"
+    sent_on Time.now
+    content_type "text/html"
+
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,6 +23,9 @@ class UserMailer < ActionMailer::Base
   #
   def password_reset(user)
     @user = user
+    from    "C.Best Ideas<notifications@example.com>"
     mail to: user.email, subject: "Password Reset Request"
+    sent_on Time.now
+    content_type "text/html"
   end
 end
